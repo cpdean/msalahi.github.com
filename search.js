9e35fb945ad1c30e60a47564e7ac9cdc595abcc1
Murad Salahi <salahi.murad@gmail.com>
2012-01-15 16:34:26 -0600

    search test

--- search.js
+++ search.js
@@ -1,61 +1,63 @@
-function helloWorld(messages){
-	this.messages = messages;
-		
-	this.doIt = function (){
-		for(var i=0; i<messages.length;i++){
-			alert(messages[i]);
+function Trie(){
+
+	/*Underlying JS Array Structure*/
+	this.dict = new Array();
+	this.dict['']=new Array();
+
+	/*Friendly Public functions*/
+	
+
+	this.addWord = function(word){
+		//Adds word to the trie by calling helper function.
+		addWordHelper(this.dict[''],word);
+	}
+
+	this.display = function(){
+		//Displays the trie in console for debugging purposes.
+		console.log("***Tree***");
+		displayHelper(this.dict[''],'','');
+	}	
+	function displayHelper(children,wordSegment,indent){
+		//Recursively prints out the trie.
+		console.log(indent,wordSegment);
+		for(child in children){
+			if(child==0){
+				console.log(indent.concat('    '),wordSegment,"(word)");
+			}else{
+				displayHelper(children[child], wordSegment.concat(child),indent.concat('    '));
+			}
 		}
 	}
-}
 
-function initialize(){
-	dict = new Array();
-	dict[''] = new Array();
-	addWord(dict[''],'cat');
-	addWord(dict[''],'crap');
-	addWord(dict[''],'cap');
-	addWord(dict[''],'dracula');
-	printTree(dict[''],'','');
-}
-
-
-function getRoot(tree){
-	for( key in tree){
-		return key;
-	}
-}
-	 
-function addWord (children,word){
-
-	if(word.length===1){
-		console.log("We're in here now.");
-		if(word in children){
-			console.log("word in children");
-			if(!('' in children[word])){
-				children.push('');
+	function addWordHelper(children,word){
+		//Recursively adds a word to the try.
+		if(word.length===1){
+			if(word in children){
+				if(!('' in children[word])){
+					children.push('');
+				}
+			}
+			else{
+				children[word] = new Array('');
 			}
 		}
 		else{
-			console.log("word not in children");
-			children[word] = new Array('');
+			if(!(word[0] in children)){
+				children[word[0]] = new Array();
+			}
+			addWordHelper(children[word[0]],word.substring(1,word.length));
 		}
-	}
-	else{
-		if(!(word[0] in children)){
-			children[word[0]] = new Array();
-		}
-		addWord(children[word[0]],word.substring(1,word.length));
+
 	}
 
+}	
+
+function initialize(){
+	var dict = new Trie();
+	dict.addWord('cat');
+	dict.addWord('crap');
+	dict.addWord('cap');
+	dict.addWord('dracula');
+	dict.display();
 }
 
-function printTree(children,wordSegment,indent){
-	console.log(indent,wordSegment);
-	for(child in children){
-		if(child==0){
-			console.log(indent.concat('    '),wordSegment,"(word)");
-		}else{
-			printTree(children[child], wordSegment.concat(child),indent.concat('    '));
-		}
-	}
-}
